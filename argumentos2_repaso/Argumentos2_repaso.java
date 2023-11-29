075231f549853af9a8ef87f24a8d877b380a5f7b
Danimeyus <151823205+Danimeyus@users.noreply.github.com>
2023-11-29 21:31:30 +0100

    Add files via upload

---
+++ argumentos2_repaso/Argumentos2_repaso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.argumentos2_repaso;
+
+/**
+ *
+ * @author Dani
+ */
+public class Argumentos2_repaso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
